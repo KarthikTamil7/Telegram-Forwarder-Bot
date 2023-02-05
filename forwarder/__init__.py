c587ed975acc9a84a33a30fa25e1c252eb8fbb12
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 12:42:41 +0530

    Update __init__.py

--- forwarder/__init__.py
+++ forwarder/__init__.py
@@ -26,9 +26,19 @@ if ENV:
         raise Exception("Your TO_CHATS list does not contain valid integers.")
 
     try:
-        TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "").split())
+        TO_CHATS = set(int(x) for x in os.environ.get("TO_CHATS", "0").split())
     except ValueError:
         raise Exception("Your TO_CHATS list does not contain valid integers.")
+
+    try:
+        FROM_CHATS_1 = set(int(x) for x in os.environ.get("FROM_CHATS_1", "0").split())
+    except ValueError:
+        raise Exception("Your FROM_CHATS_1 list does not contain valid integers.")
+
+    try:
+        TO_CHATS_1 = set(int(x) for x in os.environ.get("TO_CHATS_1", "0").split())
+    except ValueError:
+        raise Exception("Your TO_CHATS_1 list does not contain valid integers.")
 
     REMOVE_TAG = bool(os.environ.get("REMOVE_TAG", False))
     WEBHOOK = bool(os.environ.get("WEBHOOK", False))
