47ec34b310b92a2835771ebd81691d9d863a5437
KarthikTamil7 <108778191+KarthikTamil7@users.noreply.github.com>
2023-02-05 15:22:25 +0530

    Update auto_forward.py

--- forwarder/modules/auto_forward.py
+++ forwarder/modules/auto_forward.py
@@ -52,13 +52,12 @@ try:
 except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
     LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
 
-
+# Channel 1
 
 def send_message(message: Message, chat_id: int) -> Union[MessageId, Message]:
     if REMOVE_TAG:
         return message.copy(chat_id)
     return message.forward(chat_id)
-
 
 
 
@@ -95,53 +94,8 @@ try:
 
     dispatcher.add_handler(FORWARD_HANDLER)
 
-except ValueError:  # When FROM_CHATS_1 list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_1 list is empty.")
-
-# Channel 1
-
-def send_message(message: Message, chat_id: int) -> Union[MessageId, Message]:
-    if REMOVE_TAG:
-        return message.copy(chat_id)
-    return message.forward(chat_id)
-
-
-
-def forward(update: Update, context: CallbackContext):
-    message = update.effective_message
-    chat = update.effective_chat
-    if not message or not chat:
-        return
-    from_chat_name = chat.title or chat.first_name
-
-    for chat in TO_CHATS:
-        to_chat_name = (
-            context.bot.get_chat(chat).title or context.bot.get_chat(chat).first_name
-        )
-        try:
-            send_message(message, chat)
-        except ChatMigrated as err:
-            send_message(message, err.new_chat_id)
-            LOGGER.warning(f"Chat {chat} has been migrated to {err.new_chat_id}!! Edit the config file!!")
-        except:
-            LOGGER.exception(
-                'Error while forwarding message from chat "{}" to chat "{}".'.format(
-                    from_chat_name, to_chat_name
-                )
-            )
-
-
-try:
-    FORWARD_HANDLER = MessageHandler(
-        Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
-        forward,
-        run_async=True,
-    )
-
-    dispatcher.add_handler(FORWARD_HANDLER)
-
 except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
+    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_1 list is empty.")
 
 # Channel 2
 
@@ -159,7 +113,7 @@ def forward(update: Update, context: CallbackContext):
         return
     from_chat_name = chat.title or chat.first_name
 
-    for chat in TO_CHATS:
+    for chat in TO_CHATS_2:
         to_chat_name = (
             context.bot.get_chat(chat).title or context.bot.get_chat(chat).first_name
         )
@@ -178,15 +132,15 @@ def forward(update: Update, context: CallbackContext):
 
 try:
     FORWARD_HANDLER = MessageHandler(
-        Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
+        Filters.chat(FROM_CHATS_2) & ~Filters.status_update & ~Filters.command,
         forward,
         run_async=True,
     )
 
     dispatcher.add_handler(FORWARD_HANDLER)
 
-except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
+except ValueError:  # When FROM_CHATS_2 list is not set because user doesn't know chat id(s)
+    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_2 list is empty.")
 
 # Channel 3
 
@@ -204,7 +158,7 @@ def forward(update: Update, context: CallbackContext):
         return
     from_chat_name = chat.title or chat.first_name
 
-    for chat in TO_CHATS:
+    for chat in TO_CHATS_3:
         to_chat_name = (
             context.bot.get_chat(chat).title or context.bot.get_chat(chat).first_name
         )
@@ -223,15 +177,15 @@ def forward(update: Update, context: CallbackContext):
 
 try:
     FORWARD_HANDLER = MessageHandler(
-        Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
+        Filters.chat(FROM_CHATS_3) & ~Filters.status_update & ~Filters.command,
         forward,
         run_async=True,
     )
 
     dispatcher.add_handler(FORWARD_HANDLER)
 
-except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
+except ValueError:  # When FROM_CHATS_3 list is not set because user doesn't know chat id(s)
+    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_3 list is empty.")
 
 # Channel 4
 
@@ -249,7 +203,7 @@ def forward(update: Update, context: CallbackContext):
         return
     from_chat_name = chat.title or chat.first_name
 
-    for chat in TO_CHATS:
+    for chat in TO_CHATS_4:
         to_chat_name = (
             context.bot.get_chat(chat).title or context.bot.get_chat(chat).first_name
         )
@@ -268,15 +222,15 @@ def forward(update: Update, context: CallbackContext):
 
 try:
     FORWARD_HANDLER = MessageHandler(
-        Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
+        Filters.chat(FROM_CHATS_4) & ~Filters.status_update & ~Filters.command,
         forward,
         run_async=True,
     )
 
     dispatcher.add_handler(FORWARD_HANDLER)
 
-except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
+except ValueError:  # When FROM_CHATS_4 list is not set because user doesn't know chat id(s)
+    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_4 list is empty.")
 
 # Channel 5
 
@@ -294,7 +248,7 @@ def forward(update: Update, context: CallbackContext):
         return
     from_chat_name = chat.title or chat.first_name
 
-    for chat in TO_CHATS:
+    for chat in TO_CHATS_5:
         to_chat_name = (
             context.bot.get_chat(chat).title or context.bot.get_chat(chat).first_name
         )
@@ -313,12 +267,12 @@ def forward(update: Update, context: CallbackContext):
 
 try:
     FORWARD_HANDLER = MessageHandler(
-        Filters.chat(FROM_CHATS) & ~Filters.status_update & ~Filters.command,
+        Filters.chat(FROM_CHATS_5) & ~Filters.status_update & ~Filters.command,
         forward,
         run_async=True,
     )
 
     dispatcher.add_handler(FORWARD_HANDLER)
 
-except ValueError:  # When FROM_CHATS list is not set because user doesn't know chat id(s)
-    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS list is empty.")
+except ValueError:  # When FROM_CHATS_5 list is not set because user doesn't know chat id(s)
+    LOGGER.warn("I can't FORWARD_HANDLER because your FROM_CHATS_5 list is empty.")
